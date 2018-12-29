ef8856ba2a7844b1a6b40154ed8a10ab83e1544d
Vlad Mayakov <xaeronation@gmail.com>
2018-12-29 13:11:23 +0200

    Add initial spring boot template

--- generators/app/index.js
+++ generators/app/index.js
@@ -14,15 +14,43 @@ module.exports = class extends Generator {
 
     const prompts = [
       {
-        type: 'confirm',
-        name: 'someAnswer',
-        message: 'Would you like to enable this option?',
-        default: true
+        type: 'string',
+        name: 'packageName',
+        message: 'Package name',
+        default: 'net.nineseventwo'
       }, {
         type: 'string',
-        name: 'name',
-        message: 'Enter your name:',
-        default: 'John'
+        name: 'serviceName',
+        message: 'Service name',
+        default: 'sandbox'
+      }, {
+        type: 'string',
+        name: 'serviceVersion',
+        message: 'Service version',
+        default: '1.0.0'
+      }, {
+        type: 'list',
+        name: 'javaVersion',
+        message: 'Java version',
+        choices: [
+          {
+            name: 'Java 8',
+            value: '1.8',
+          }, {
+            name: 'Java 9',
+            value: '1.9'
+          }
+        ]
+      }, {
+        type: 'list',
+        name: 'springBootVersion',
+        message: 'Spring Boot version:',
+        choices: [
+          {
+            name: '1.5.18.RELEASE',
+            value: '1.5.18.RELEASE'
+          }
+        ]
       }
     ];
 
@@ -33,12 +61,52 @@ module.exports = class extends Generator {
   }
 
   writing() {
+    var packageTplPath = 'net/nineseventwo/template'
+    var packagePath = this.props.packageName.replace(/\./g, '/') + '/' + this.props.serviceName;
+    var gradleWrapperPath = 'gradle/wrapper'
+    var srcMainJavaPath = 'src/main/java'
+    var srcMainResourcesPath = 'src/main/resources'
+    var srcMainTestPath = 'src/test/java'
+
+    this.fs.copy(this.templatePath('.gitignore'), this.destinationPath('.gitignore'));
+
     this.fs.copyTpl(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt'), {
-        someAnswer: this.props.someAnswer,
-        name: this.props.name
-      }
-    );
+      this.templatePath('build.gradle'),
+      this.destinationPath('build.gradle'), {
+        packageName: this.props.packageName,
+        serviceName: this.props.serviceName,
+        serviceVersion: this.props.serviceVersion,
+        javaVersion: this.props.javaVersion
+      });
+
+    this.fs.copyTpl(
+      this.templatePath('settings.gradle'),
+      this.destinationPath('settings.gradle'), {
+        packageName: this.props.packageName,
+        serviceName: this.props.serviceName,
+        serviceVersion: this.props.serviceVersion,
+        javaVersion: this.props.javaVersion
+      });
+
+    this.fs.copy(this.templatePath('gradlew'), this.destinationPath('gradlew'), this);
+    this.fs.copy(this.templatePath('gradlew.bat'), this.destinationPath('gradlew.bat'), this);
+    this.fs.copy(this.templatePath(gradleWrapperPath + '/gradle-wrapper.jar'), this.destinationPath(gradleWrapperPath + '/gradle-wrapper.jar'), this);
+    this.fs.copy(this.templatePath(gradleWrapperPath + '/gradle-wrapper.properties'), this.destinationPath(gradleWrapperPath + '/gradle-wrapper.properties'), this);
+
+
+    this.fs.copyTpl(
+      this.templatePath(srcMainJavaPath + '/' + packageTplPath + '/DemoApplication.java'),
+      this.destinationPath(srcMainJavaPath + '/' + packagePath + '/DemoApplication.java'), {
+        packageName: this.props.packageName,
+        serviceName: this.props.serviceName
+      });
+    this.fs.copy(this.templatePath(srcMainResourcesPath + '/application.yaml'), this.destinationPath(srcMainResourcesPath + '/application.yaml'), this);
+    this.fs.copyTpl(
+      this.templatePath(srcMainTestPath + '/' + packageTplPath + '/DemoApplicationTests.java'),
+      this.destinationPath(srcMainTestPath + '/' + packagePath + '/DemoApplicationTests.java'), {
+        packageName: this.props.packageName,
+        serviceName: this.props.serviceName
+      });
   }
+
 };
